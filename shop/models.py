ff9d219cb4cc13ce5f0da6075992c5de956ce5cc
Chinmaya Kumar Sahoo <ChSahoo@ESTUATE.COM>
2023-12-23 00:09:33 +0530

    Added show_items function based view also added pagination

--- shop/models.py
+++ shop/models.py
@@ -11,3 +11,6 @@ class Product(models.Model):
     catagory = models.CharField(max_length=100)
     rating = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(5)])
     image = models.CharField(max_length=500)
+
+    def __str__(self) -> str:
+        return self.title
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
+from .models import Product
 # Create your views here.
+def show_items(request):
+    items = Product.objects.all()
+
+    paginator = Paginator(items, 4)
+    page_num = request.GET.get('page')
+    item_object = paginator.get_page(page_num)
+    return render(request, 'shop/index.html', {"items":item_object})
