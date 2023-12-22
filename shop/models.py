f079afce349ee687e5b4a71029ae22b593bc30dd
Chinmaya Kumar Sahoo <ChSahoo@ESTUATE.COM>
2023-12-22 22:49:54 +0530

    Created a app called shop and created a model in it with the name Product

---
+++ shop/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    catagory = models.CharField(max_length=100)
+    rating = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    image = models.CharField(max_length=500)
