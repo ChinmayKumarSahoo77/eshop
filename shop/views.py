136040aacddc32c7eeabd1b7e0aa2fa6df41a14e
Chinmaya Kumar Sahoo <ChSahoo@ESTUATE.COM>
2023-12-24 11:38:59 +0530

    renamed the details view to detail view

--- shop/views.py
+++ shop/views.py
@@ -7,10 +7,12 @@ from django.contrib import messages
 def show_items(request):
     items = Product.objects.all().order_by('title')
     
+    # Search functionality
     search_val = request.GET.get('search')
     if search_val != "" or search_val is not None:
         items = items.filter(title__icontains = search_val)
 
+    # Pagination functionality
     paginator = Paginator(items, 4)
     page_num = request.GET.get('page')
     item_object = paginator.get_page(page_num)
@@ -21,4 +23,4 @@ def detail_view(request, id: int):
     item_obj = Product.objects.get(id = id)
     
     messages.success(request, f"Item details with id {id} fetched successfully.")
-    return render(request, 'shop/details.html', {'item_obj':item_obj})
+    return render(request, 'shop/detail.html', {'item_obj':item_obj})
